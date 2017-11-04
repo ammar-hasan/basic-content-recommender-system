e04353bc8796d835231672ae76f1b97e1c0f7e0d
Ammar Hasan <ammar_rizvi@msn.com>
2017-11-05 00:06:31 +0500

    updated key

--- stage-4/mapper-4-make-recommendation.py
+++ stage-4/mapper-4-make-recommendation.py
@@ -14,6 +14,6 @@ Algorithm:
 
 for row in csv.reader(iter(sys.stdin.readline, '')):
   writer = csv.writer(sys.stdout, quoting=csv.QUOTE_NONNUMERIC)
-  key = '%s-%s-%s' % (row[4], row[5], row[0])
+  key = '%s-%s-%s' % (row[4].strip(), row[5].strip(), row[0].strip())
   row.insert(0, key)
   writer.writerow(row)

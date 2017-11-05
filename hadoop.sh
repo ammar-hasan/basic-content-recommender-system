0e529d2d8e5d51f40ac325c31b12d668a94ab980
Ammar Hasan <ammar_rizvi@msn.com>
2017-11-05 11:30:41 +0500

    Update

--- hadoop.sh
+++ hadoop.sh
@@ -24,6 +24,12 @@ hadoop jar $HDJAR_PATH \
  -output /reco/output/stage-1 \
  1>> logs/out.log 2>> logs/err.log;
 
+SUCCESS=$(hadoop fs -ls /reco/output/stage-1/_SUCCESS 2>> logs/err.log);
+if [ -z "$SUCCESS" ]; then
+	echo "Stage-1 has failed, check logs.";
+	exit;
+fi
+
 echo "Executing stage-2 (Joining movies with ratings), result will go to /reco/output/stage-2 in HDFS.";
 hadoop jar $HDJAR_PATH \
  -mapper $PWD/stage-2/mapper-2-join-ratings.py \
@@ -33,6 +39,12 @@ hadoop jar $HDJAR_PATH \
  -output /reco/output/stage-2 \
  1>> logs/out.log 2>> logs/err.log;
 
+SUCCESS=$(hadoop fs -ls /reco/output/stage-2/_SUCCESS 2>> logs/err.log);
+if [ -z "$SUCCESS" ]; then
+	echo "Stage-2 has failed, check logs.";
+	exit;
+fi
+
 echo "Executing stage-3 (Calculating recommendations), result will go to /reco/output/stage-3 in HDFS.";
 hadoop jar $HDJAR_PATH \
  -mapper $PWD/stage-3/mapper-3-recommend-movie.py \
@@ -40,6 +52,12 @@ hadoop jar $HDJAR_PATH \
  -input /reco/output/stage-2 \
  -output /reco/output/stage-3 \
  1>> logs/out.log 2>> logs/err.log;
+
+SUCCESS=$(hadoop fs -ls /reco/output/stage-3/_SUCCESS 2>> logs/err.log);
+if [ -z "$SUCCESS" ]; then
+	echo "Stage-3 has failed, check logs.";
+	exit;
+fi
 
 echo "Executing stage-4 (Final Stage: Make recommendations), result will go to /reco/output/stage-4 in HDFS.";
 hadoop jar $HDJAR_PATH \
@@ -51,9 +69,10 @@ hadoop jar $HDJAR_PATH \
  -output /reco/output/stage-4 \
  1>> logs/out.log 2>> logs/err.log;
 
-SUCCESS=$(hadoop fs -ls /reco/output/stage-4/_SUCCESS)
+SUCCESS=$(hadoop fs -ls /reco/output/stage-4/_SUCCESS 2>> logs/err.log);
 if [ -z "$SUCCESS" ]; then
-	echo "Program has failed during execution, check logs.";
+	echo "Stage-4 has failed, check logs.";
+	exit;
 fi
 
 echo "Downloading the output file from HDFS to $PWD/output/recos.csv";
--- recommend.sh
+++ recommend.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "Executing stage-1 (Calculating average ratings), result will go to ${PWD}/output/stage-1-out.txt";
 cat "data/ratings.csv" | python "stage-1/mapper-1-average-ratings.py" | sort | python "stage-1/reducer-1-average-ratings.py" > "output/stage-1-out.txt";
+echo "Executing stage-2 (Joining movies with ratings), result will go to ${PWD}/output/stage-2-out.txt";
 cat "data/movies.csv" "output/stage-1-out.txt" | python "stage-2/mapper-2-join-ratings.py" | sort | python "stage-2/reducer-2-join-ratings.py" > "output/stage-2-out.txt";
+echo "Executing stage-3 (Calculating recommendations), result will go to ${PWD}/output/stage-3-out.txt";
 cat "output/stage-2-out.txt" | python "stage-3/mapper-3-recommend-movie.py" | sort | python "stage-3/reducer-3-recommend-movie.py" > "output/stage-3-out.txt";
+echo "Executing stage-4 (Final Stage: Make recommendations), result will go to ${PWD}/output/final-out.txt";
 cat "output/stage-3-out.txt" | python "stage-4/mapper-4-make-recommendation.py" | sort -r | python "stage-4/reducer-4-make-recommendation.py" > "output/final-out.txt";
